9ad38acf962da4ed209c5f2b107d26b4151cac73
Hassan Murtaza <mrhassanmurtaza@gmail.com>
2020-08-15 18:13:20 +0500

    beautify code

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 const fs = require('fs')
 const AWS = require('aws-sdk')
 
-AWS.config.update({accessKeyId: '', 
-secretAccessKey: '', 
-region:''});
+AWS.config.update({
+	accessKeyId: '',
+	secretAccessKey: '',
+	region: ''
+});
 
-const secretsmanager = new AWS.SecretsManager({apiVersion: '2017-10-17'});
+const secretsmanager = new AWS.SecretsManager({
+	apiVersion: '2017-10-17'
+});
 const sourcePath = process.argv.slice(2)[0] || '.env'
 
 function parseFileSync(filePath) {
@@ -33,13 +37,13 @@ function parseSync(src) {
 		const result = {}
 		const lines = src.toString().split('\n')
 		for (const line of lines) {
-      const match = line.match(/^([^=:]+?)[=:](.*)/)
+			const match = line.match(/^([^=:]+?)[=:](.*)/)
 			if (match) {
 				let key = match[1].trim()
-        let value = match[2].trim()
-        if ( value.charAt(0) == '\''){
-          value = value.slice(1, -1)
-        }
+				let value = match[2].trim()
+				if (value.charAt(0) == '\'') {
+					value = value.slice(1, -1)
+				}
 				result[key] = value
 			}
 		}
@@ -51,27 +55,27 @@ function isError(value) {
 	return value instanceof Error
 }
 
-function storeJsonToFile(jsonContent){
-  fs.writeFile("output.json",  JSON.stringify(jsonContent), 'utf8', function (err) {
-    if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
-    }
- 
-    console.log("JSON file has been saved.");
-}); 
+function storeJsonToFile(jsonContent) {
+	fs.writeFile("output.json", JSON.stringify(jsonContent), 'utf8', function (err) {
+		if (err) {
+			console.log("An error occured while writing JSON Object to File.");
+			return console.log(err);
+		}
+
+		console.log("JSON file has been saved.");
+	});
 }
 
-function createSecret(env_json){
-  var params = {
-    Description: "My test database secret created with the SDK", 
-    Name: "TEST_SECRET", 
-    SecretString: JSON.stringify(env_json)
-  };
-  secretsmanager.createSecret(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-  });
+function createSecret(env_json) {
+	var params = {
+		Description: "My test database secret created with the SDK",
+		Name: "TEST_SECRET",
+		SecretString: JSON.stringify(env_json)
+	};
+	secretsmanager.createSecret(params, function (err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else console.log(data); // successful response
+	});
 }
 
 
